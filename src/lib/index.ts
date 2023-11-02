6d62a6707103ba3bd53475b9c72fd0b61f80d8b1
Tyler Smith <tysmith@ea.com>
2023-11-02 07:42:55 -0700

    Fix fonts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,30 +1,31 @@
-import '$lib/assets/fonts/fontawesome/css/fontawesome.min.css';
-import '$lib/assets/fonts/fontawesome/css/regular.min.css';
-import '$lib/assets/fonts/roboto/css/roboto.css';
+import './assets/fonts/archivo/css/archivo.css';
+import './assets/fonts/fontawesome/css/fontawesome.min.css';
+import './assets/fonts/fontawesome/css/regular.min.css';
+import './assets/fonts/roboto/css/roboto.css';
 
 export * from '$lib/components/buttons/button/button.constants.js';
-export * from '$lib/components/buttons/button/button.svelte';
+export { default as Button } from '$lib/components/buttons/button/button.svelte';
 export * from '$lib/components/buttons/button/button.types.js';
-export * from '$lib/components/buttons/floating-action-button/floating-action-button.svelte';
+export { default as FloatingActionButton } from '$lib/components/buttons/floating-action-button/floating-action-button.svelte';
 
 export * from '$lib/components/icon/icon.constants.js';
-export * from '$lib/components/icon/icon.svelte';
+export { default as Icon } from '$lib/components/icon/icon.svelte';
 export * from '$lib/components/icon/icon.types.js';
 
 export * from '$lib/components/inputs/text-input/text-input.constants.js';
-export * from '$lib/components/inputs/text-input/text-input.svelte';
+export { default as TextInput } from '$lib/components/inputs/text-input/text-input.svelte';
 export * from '$lib/components/inputs/text-input/text-input.types.js';
-export * from '$lib/components/inputs/toggle-buttons/toggle-buttons.svelte';
+export { default as ToggleButtons } from '$lib/components/inputs/toggle-buttons/toggle-buttons.svelte';
 export * from '$lib/components/inputs/toggle-buttons/toggle-buttons.types.js';
 
-export * from '$lib/components/layout/button-group/button-group.svelte';
+export { default as ButtonGroup } from '$lib/components/layout/button-group/button-group.svelte';
 export * from '$lib/components/layout/modal/modal.svelte';
 
 export * from '$lib/components/typography/display-text/display-text.constants.js';
-export * from '$lib/components/typography/display-text/display-text.svelte';
+export { default as DisplayText } from '$lib/components/typography/display-text/display-text.svelte';
 export * from '$lib/components/typography/display-text/display-text.types.js';
 export * from '$lib/components/typography/heading/heading.constants.js';
-export * from '$lib/components/typography/heading/heading.svelte';
+export { default as Heading } from '$lib/components/typography/heading/heading.svelte';
 export * from '$lib/components/typography/heading/heading.types.js';
 
 export * from '$lib/stores/tokens/tokens.constants.js';
